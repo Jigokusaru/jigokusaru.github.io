95ee631d0abaea6437964b54a78cef7e641614f8
Tim Joubert <Jigokusaru@users.noreply.github.com>
2018-01-27 19:56:41 -0500

    Added the files

---
+++ js/test.js
@@ -0,0 +1,33 @@
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+function checkBday() {
+    today = new Date()
+    var bday=new Date(document.getElementById("bday").value); //Month is 0-11 in JavaScript
+      //Set 1 day in milliseconds
+    var one_yr=1000*60*60*24 * 30 * 12
+    age = Math.floor((bday.getTime() - today.getTime() ) / one_yr);
+    if (age <= 21) {
+        y = true
+    }else{
+        y = false
+    }
+    alert(y);
+}
